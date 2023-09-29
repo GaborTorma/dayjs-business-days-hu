f8c98bd081a844f41c89d64f22e9bcb17f869fea
Gábor Torma <gabor@torma.dev>
2023-09-29 16:22:33 +0200

    fix: holiday test date

--- tests/test.ts
+++ tests/test.ts
@@ -9,7 +9,7 @@ describe('.isBusinessDay (2022-10-15 is a forced working day)', function () {
 
 describe('.isHoliday (2022-11-01 is a holiday)', function () {
 	it('should be true', function (done) {
-		expect(dayjs('2022-01-01').isHoliday()).toBe(true)
+		expect(dayjs('2022-11-01').isHoliday()).toBe(true)
 		done()
 	})
 })
